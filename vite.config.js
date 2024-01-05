5cc982a53f170d9be5282c027ff77448d3a5c839
Sudipta Dhawya <engineersudipta2021@gmail.com>
2024-01-05 15:50:59 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Normal-table/"
+  base:"/Normal1-table/",
   plugins: [react()],
 })
